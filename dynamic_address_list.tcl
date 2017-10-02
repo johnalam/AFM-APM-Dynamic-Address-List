8875973b495a2d4da257a4075983bcbb55ce3bcc
johnalam <joalam@verizon.net>
2017-10-02 11:59:21 -0400

    Pull user group info from APM session variables and send to ILXServer node.js plugin
    The ILX::call between TCL and the node.js plugin is modified to include one additional parameter.  This parameter is the address list name which reflects the user group name.  The group name in pulled from the APM session variable "session.cust.user.group".  Use the APM VPE agents to create and populate this variable.  It can contain a fixed value or value derived from the results of an LDAP/AD query.

--- dynamic_address_list.tcl
+++ dynamic_address_list.tcl
@@ -3,13 +3,16 @@
 #
 
 # add the address to the address list
+
 when ACCESS_POLICY_COMPLETED {
-	set ilx_handle [ILX::init "dynamic_address_list_plugin" "dynamic_address_list"] 
-	# set IP address
-    set ip_address [ACCESS::session data get session.user.clientip] 
+    set ilx_handle [ILX::init "dynamic_address_list_plugin" "dynamic_address_list"] 
+    # set IP address
+    set ip_address [ACCESS::session data get session.user.clientip]
+    set group [ACCESS::session data get session.cust.user.group]
+    set address_list "apm_accesslist_$group"
     if {[info exists ip_address]} {
 	    # create a random secret
-	    if {[catch {set res [ILX::call $ilx_handle "addAddress" $ip_address]} result]} {
+	    if {[catch {set res [ILX::call $ilx_handle "addAddress" $ip_address $address_list]} result]} {
 	        log local0.error "Client - [IP::client_addr], ILX failure: $result"
 	        return
 	    }
@@ -21,12 +24,14 @@ when ACCESS_POLICY_COMPLETED {
 
 # remove the address from the address list
 when ACCESS_SESSION_CLOSED {
-	set ilx_handle [ILX::init "dynamic_address_list_plugin" "dynamic_address_list"] 
+    set ilx_handle [ILX::init "dynamic_address_list_plugin" "dynamic_address_list"] 
 	# set IP address
     set ip_address [ACCESS::session data get session.user.clientip] 
+    set group [ACCESS::session data get session.cust.user.group]
+    set address_list "apm_accesslist_$group"
     if {[info exists ip_address]} {
 	    # create a random secret
-	    if {[catch {set res [ILX::call $ilx_handle "deleteAddress" $ip_address]} result]} {
+	    if {[catch {set res [ILX::call $ilx_handle "deleteAddress" $ip_address $address_list]} result]} {
 	        log local0.error "Client - $ip_address, ILX failure: $result"
 	        return
 	    }
@@ -35,3 +40,5 @@ when ACCESS_SESSION_CLOSED {
     	return
     }
 }
+
+
